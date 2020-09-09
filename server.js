c27af23adfbf843ca2c7becc9d09030fe92d3cac
Atul Anand <62374512+Atul0419-223@users.noreply.github.com>
2020-09-09 13:50:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,5 +5,7 @@ const PORT = process.env.port || 3000;
 const app = express();
 
 app.use(express.static(__dirname + '/build'));
-
+app.get('*', (req, res) => {
+   res.sendFile(path.join(__dirname + '/build', 'index.html'));
+});
 app.listen(PORT, () => console.log(`listening at ${PORT}`));
